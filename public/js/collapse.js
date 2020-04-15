78ec43f07007d12a4f2892f1cb50b0cce357ca79
michaelrodrigue <scottlovos503@gmail.com>
2020-04-15 09:49:43 -0600

    contenedor navbar

--- public/js/collapse.js
+++ public/js/collapse.js
@@ -1,9 +1,38 @@
 $(document).ready(function () {
-    $(".navbar-toggler").click(function(){
+   toggle();
+
+  
+   $(".contenedor").load("public/vistas/inicio/inicio.html")
+
+
+   
+   $('#home').click(()=>{
+	   $(".contenedor").load("public/vistas/inicio/inicio.html");
+   
+   });
+   
+   $('#verduras').click(()=>{
+	   $(".contenedor").load("public/vistas/verduras/verduras.html");
+   
+   });
+  
+   $('#frutos').click(()=>{
+	   $(".contenedor").load("public/vistas/frutos/frutos.html");
+   
+   });
+  
+	  
+
+
+
+
+});
+function toggle(){
+	$(".navbar-toggler").click(function(){
 		console.log("click");
 		
         $(".collapse").animate({
 			height: 'toggle'
 		  });
-      });
-});
+	  });
+}
