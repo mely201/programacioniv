3891e0db2f3bcaae11e09d61ddd28ce938f8c79f
michaelrodrigue <scottlovos503@gmail.com>
2020-04-17 12:12:29 -0600

    registro

--- public/vistas/login/login.js
+++ public/vistas/login/login.js
@@ -1,4 +1,25 @@
-$(document).ready(function () {
+var registrarusuarios =new vue({
+	el:'#signupbox',
+	data:{
+		usuario:{
+			idusuario	:	0,
+			accion		: 	'nuevo',
+			nombre		:   'Nombre',
+			apellido	:	'Apellido',
+			correo		:	'Correo',
+			password	:	'password',
+			direccion	:	'Direccion',
+			msg			:	''
 
-	
+		}
+	},
+	method:{
+		registrarusuario(){
+			fetch(`private/Modulos/usuarios/procesos.php?proceso=recibirDatos&usuario=${JSON.stringify(this.usuario)}`).then( resp=>resp.json() ).then(resp=>{
+                this.usuario.msg = resp.msg;
+            });
+		}
+	}
+
+
 });
