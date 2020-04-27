98456eb80426a4365adac439e51f36fa431ffdaf
michaelrodrigue <scottlovos503@gmail.com>
2020-04-27 14:50:33 -0600

    casi fin

--- Private/Modulos/alquiler/procesos.php
+++ Private/Modulos/alquiler/procesos.php
@@ -1,85 +1,119 @@
-<?php 
+<?php
 include('../../Config/Config.php');
-$docente = new docente($Conexion);
+$alquiler = new alquiler($Conexion  );
 
 $proceso = '';
 if( isset($_GET['proceso']) && strlen($_GET['proceso'])>0 ){
 	$proceso = $_GET['proceso'];
 }
-$docente->$proceso( $_GET['docente'] );
-print_r(json_encode($docente->respuesta));
+$alquiler->$proceso( $_GET['alquiler'] );
+print_r(json_encode($alquiler->respuesta));
 
-class docente{
+class alquiler{
     private $datos = array(), $db;
     public $respuesta = ['msg'=>'correcto'];
     
     public function __construct($db){
         $this->db=$db;
     }
-    public function recibirDatos($docente){
-        $this->datos = json_decode($docente, true);
+    public function recibirDatos($alquiler){
+        $this->datos = json_decode($alquiler, true);
         $this->validar_datos();
     }
     private function validar_datos(){
-        if( empty($this->datos['codigo']) ){
-            $this->respuesta['msg'] = 'por favor ingrese el codigo del  Docente';
+        if( empty($this->datos['cliente']['id']) ){
+            $this->respuesta['msg'] = 'por favor ingrese el cliente del alquiler';
         }
-        if( empty($this->datos['nombre']) ){
-            $this->respuesta['msg'] = 'por favor ingrese el nombre del  Docente';
+        if( empty($this->datos['pelicula']['id']) ){
+            $this->respuesta['msg'] = 'por favor ingrese el pelicula';
         }
-        if( empty($this->datos['direccion']) ){
-            $this->respuesta['msg'] = 'por favor ingrese la direccion del Docente';
-        }
-       
-        $this->almacenar_docente();
+        $this->almacenar_alquiler();
     }
-    private function almacenar_docente(){
+    private function almacenar_alquiler(){
         if( $this->respuesta['msg']==='correcto' ){
             if( $this->datos['accion']==='nuevo' ){
                 $this->db->consultas('
-                    INSERT INTO docentes (codigo,nombre,direccion,telefono,nit) VALUES(
-                        "'. $this->datos['codigo'] .'",
-                        "'. $this->datos['nombre'] .'",
-                        "'. $this->datos['direccion'] .'",
-                        "'. $this->datos['telefono'] .'",
-                        "'. $this->datos['nit'] .'"
+                    INSERT INTO alquiler (idcliente,idpelicula,fechaprestamo,fechadevoluion,valor) VALUES(
+                        "'. $this->datos['cliente']['id'] .'",
+                        "'. $this->datos['pelicula']['id'] .'",
+                        "'. $this->datos['fechaP'] .'",
+                        "'. $this->datos['fechaD'] .'",
+                        "'. $this->datos['valor'] .'"
                     )
                 ');
                 $this->respuesta['msg'] = 'Registro insertado correctamente';
             } else if( $this->datos['accion']==='modificar' ){
                 $this->db->consultas('
-                   UPDATE docentes SET
-                        codigo     = "'. $this->datos['codigo'] .'",
-                        nombre     = "'. $this->datos['nombre'] .'",
-                        direccion  = "'. $this->datos['direccion'] .'",
-                        telefono   = "'. $this->datos['telefono'] .'",
-                         nit       = "'. $this->datos['nit'] .'"
-                    WHERE idDocente = "'. $this->datos['idDocente'] .'"
+                    UPDATE alquiler SET
+                        idcliente     = "'. $this->datos['cliente']['id'] .'",
+                        idpelicula      = "'. $this->datos['peliculas']['id'] .'",
+                        fechaprestamo         = "'. $this->datos['fechaP'] .'",
+                        fechadevolucion         = "'. $this->datos['fechaD'] .'",
+                        valor         = "'. $this->datos['valor'] .'"
+                    WHERE idalquiler = "'. $this->datos['idalquiler'] .'"
                 ');
                 $this->respuesta['msg'] = 'Registro actualizado correctamente';
             }
         }
     }
-    public function buscarDocente($valor=''){
+    public function buscaralquiler($valor = ''){
+        if( substr_count($valor, '-')===2 ){
+            $valor = implode('-', array_reverse(explode('-',$valor)));
+        }
         $this->db->consultas('
-            select docentes.idDocente, docentes.codigo, docentes.nombre, docentes.direccion, docentes.telefono, docentes.nit
-            from docentes
-            where docentes.codigo like "%'.$valor.'%" or docentes.nombre like "%'.$valor.'%" or docentes.nit like "%'.$valor.'%"
+            select alquiler.idalquiler, alquiler.idcliente, alquiler.idpelicula, 
+                alquiler.fechaprestamo, alquiler.fechadevolucion, 
+                peliculas.descripcion, peliculas.sinopsis, 
+                cliente.nombre,alquiler.valor
+            from alquiler
+                inner join peliculas on(peliculas.idpelicula=alquiler.idpelicula)
+                inner join cliente on(cliente.idcliente=alquiler.idcliente)
+            where peliculas.descripcion like "%'. $valor .'%" or 
+                cliente.nombre like "%'. $valor .'%" or 
+                alquiler.fechaprestamo like "%'. $valor .'%" or
+                alquiler.fechadevolucion like "%'.$valor.'%"
         ');
-        return $this->respuesta = $this->db->obtener_datos();
+        $alquiler = $this->respuesta = $this->db->obtener_data();
+        foreach ($alquiler as $key => $value) {
+            $datos[] = [
+                'idalquiler' => $value['idalquiler'],
+                'peliculas'      => [
+                    'id'      => $value['idpelicula'],
+                    'label'   => $value['desc']
+                ],
+                'cliente'      => [
+                    'id'      => $value['idcliente'],
+                    'label'   => $value['nombre']
+                ],
+                'fechaP'       => $value['fP'],
+                'fP'           => $value['fechaP'],
+                'fechaD'        =>$value['fechaD']
+
+
+            ]; 
+        }
+        return $this->respuesta = $datos;
     }
-    public function eliminarDocente($idDoncente=''){
-    
-            $this->db->consultas( '
-                delete docentes
-                from docentes
-                where docentes.idDocente = "'.$idDoncente.'"
-            ');
-           
-                $this->respuesta['msg'] = 'Registro eliminado correctamente';
-      
+    public function traer_cliente_pelicula(){
+        $this->db->consultas('
+            select cliente.nombre AS label, cliente.idcliente AS id
+            from cliente
+        ');
+        $cliente = $this->db->obtener_datos();
+        $this->db->consultas('
+            select peliculas.descripcion AS label, peliculas.idpelicula AS id
+            from peliculas
+        ');
+        $pelicula = $this->db->obtener_datos();
+        return $this->respuesta = ['clientes'=>$cliente, 'peliculas'=>$pelicula ];//array de php en v7+
     }
- 
-    
+    public function eliminaralquiler($idalquiler = 0){
+        $this->db->consultas('
+            DELETE alquiler
+            FROM alquiler
+            WHERE alquiler.idalquiler="'.$idalquiler.'"
+        ');
+        return $this->respuesta['msg'] = 'Registro eliminado correctamente';;
+    }
 }
 ?>
--- public/vistas/alquiler/alquiler.js
+++ public/vistas/alquiler/alquiler.js
@@ -1,25 +1,42 @@
+Vue.component('v-select', VueSelect.VueSelect);
+
 var appalquiler = new Vue({
     el:'#frm-alquiler',
     data:{
         alquiler:{
-            idalquiler  : 0,
-            accion      : 'nuevo',
-           fechaP       : '',
-           fechaD       :'',
-            msg         : ''
-        }
+            idAlquiler : 0,
+            accion    : 'nuevo',
+            cliente   : {
+                idCliente : 0,
+                nombre   : ''
+            },
+            pelicula    : {
+                idPelicula : 0,
+                descripcion   : ''
+            },
+            fechaP     : '',
+            fechD      :'',
+            msg       : ''
+        },
+        clientes : {},
+        peliculas  : {}
     },
     methods:{
-        guardaralquiler:function(){
-            fetch(`Private/Modulos/alquiler/procesos.php?proceso=recibirDatos&alquiler=${JSON.stringify(this.alquiler)}`).then( resp=>resp.json() ).then(resp=>{
+        guardarAlquiler(){
+            fetch(`private/Modulos/alquiler/procesos.php?proceso=recibirDatos&alquiler=${JSON.stringify(this.alquiler)}`).then( resp=>resp.json() ).then(resp=>{
                 this.alquiler.msg = resp.msg;
-                this.alquiler.idalquiler = 0;
-                this.alquiler.fechaP = '';
-                this.alquiler.fechaD = '';
-               
-                this.alquiler.accion = 'nuevo';
-                
             });
+        },
+        limpiarAlquiler(){
+            this.alquiler.idAlquiler=0;
+            this.alquiler.accion="nuevo";
+            this.alquiler.msg="";
         }
+    },
+    created(){
+        fetch(`private/Modulos/alquiler/procesos.php?proceso=traer_cliente_pelicula&alquiler=''`).then( resp=>resp.json() ).then(resp=>{
+            this.clientes = resp.clientes;
+            this.peliculas = resp.peliculas;
+        });
     }
 });
