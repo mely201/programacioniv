f30614761f3e6836712cc599a5e0d206be1e05e8
michaelrodrigue <scottlovos503@gmail.com>
2020-04-27 09:07:53 -0600

    update

--- public/vistas/login/login.js
+++ public/vistas/login/login.js
@@ -13,7 +13,7 @@ var applogin = new Vue({
 		 inicioSesion:function(){
 			fetch(`private/Modulos/usuarios/procesos.php?proceso=recibirUsuario&login=${JSON.stringify(this.name)}`).then( resp=>resp.json() ).then(resp=>{
 				if(resp.msg=="Bienvenido"){
-					location.href='index.html';
+					location.href='cooperativa.html';
 				}else{
 					this.name.correo='';
 					this.name.pass='';
